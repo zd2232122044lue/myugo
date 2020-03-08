76f6bda51cc9b79460b66868d5827d08802d03ee
小孩 <2232122044@qq.com>
2020-03-08 17:15:59 +0800

    搜索栏状态切换

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,27 +1,60 @@
 <template>
-  <view class="search">
-    <!-- 搜索栏标签 -->
+<!-- 监听输入框事件,获取焦点时,输入框背景变成灰色 -->
+  <view class="search" :class="{focused: isFocused}">
+    <!-- 搜索框 -->
     <view class="input-box">
-      <input type="text" />
+      <input type='text' @focus="goSearch"/>
+      <text class="cancel" @click="handleCancel">取消</text>
     </view>
-    <!-- 搜索的结果 -->
+    <!-- 搜索结果 -->
     <view class="content"></view>
   </view>
 </template>
-
 <script>
-export default {};
-</script>
-
-<style lang="less" scoped>
-.search {
-  .input-box {
-    background-color: #ff2d4a;
-    padding: 20rpx 16rpx;
-    input {
-      background-color: #fff;
+export default {
+  data () {
+    return {
+        isFocused: false
     }
+  },
+  methods:{
+      goSearch(){
+          // 当输入框获取焦点时,在父元素上添加一个类名
+          this.isFocused = true
+      },
+      handleCancel(){
+          // 点击取消按钮,搜索栏恢复初始状态
+          this.isFocused = false
+      }
   }
 }
+</script>
+<style lang='less'>
+  .search {
+    .input-box {
+      display: flex;
+      background-color: #FF2D4A;
+      padding: 20rpx 16rpx;
+      input {
+        background-color: #fff;
+        flex: 1;
+      }
+      .cancel {
+          display: none;
+      }
+    }
+    &.focused {
+        .input-box {
+            background-color: #eee;
+        }
+        .cancel {
+            display: block;
+            width: 100rpx;
+            height: 50rpx;
+            line-height: 50rpx;
+            text-align: center;
+            font-size: 30rpx;
+        }
+    }
+  }
 </style>
-  
