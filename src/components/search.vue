e4a3c6705e3d63a43b7971361a00ef18d071573c
小孩 <2232122044@qq.com>
2020-03-08 17:52:24 +0800

    搜索栏内部样式和提示信息

--- src/components/search.vue
+++ src/components/search.vue
@@ -3,7 +3,7 @@
   <view class="search" :class="{focused: isFocused}">
     <!-- 搜索框 -->
     <view class="input-box">
-      <input type='text' @focus="goSearch"/>
+      <input type='text' @focus="goSearch" :placeholder="placeholder"/>
       <text class="cancel" @click="handleCancel">取消</text>
     </view>
     <!-- 搜索结果 -->
@@ -14,38 +14,88 @@
 export default {
   data () {
     return {
-        isFocused: false
+        isFocused: false,
+        placeholder:''
     }
   },
   methods:{
       goSearch(){
           // 当输入框获取焦点时,在父元素上添加一个类名
           this.isFocused = true
+          this.placeholder = "请输入"
       },
       handleCancel(){
           // 点击取消按钮,搜索栏恢复初始状态
           this.isFocused = false
+          this.placeholder = ''
       }
   }
 }
 </script>
 <style lang='less'>
   .search {
+    .content {
+        position: absolute;
+        top: 95rpx;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 9;
+        background-color: #fff;
+        display: none;
+    }
     .input-box {
       display: flex;
+      position: relative;
       background-color: #FF2D4A;
       padding: 20rpx 16rpx;
       input {
         background-color: #fff;
+        padding-left: 55rpx;
+        height: 60rpx;
         flex: 1;
       }
       .cancel {
           display: none;
       }
+
+      &::before {
+          content: '';
+          display: none;
+          width: 32rpx;
+          height: 32rpx;
+          position: absolute;
+          top: 50%;
+          transform: translate(15rpx,-50%);
+          background-image: url('http://static.botue.com/ugo/images/icon_search%402x.png');
+          background-size: 32rpx;
+      }
+      &::after {
+          content: '搜索';
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          padding-left: 40rpx;
+          transform: translate(-50%,-50%);
+          font-size: 24rpx;
+          color: #ccc;
+          background-image: url('http://static.botue.com/ugo/images/icon_search%402x.png');
+          background-size: 32rpx;
+          background-repeat: no-repeat;
+      }
     }
     &.focused {
+        .content {
+            display: block;
+        }
         .input-box {
             background-color: #eee;
+            &::after {
+                display: none;
+            }
+            &::before {
+                display: block;
+            }
         }
         .cancel {
             display: block;
