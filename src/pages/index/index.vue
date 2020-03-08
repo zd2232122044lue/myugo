1b457ecc7676d064e8b3b65bf4ded1a7b05de35c
小孩 <2232122044@qq.com>
2020-03-08 13:09:06 +0800

    主页-楼层商品

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -38,6 +38,85 @@
 				<image src="/static/uploads/icon_index_nav_4@2x.png" />
 			</navigator>
 		</view>
+
+		<!-- 楼层商品 -->
+		<view class="floors">
+			<!-- 时尚女装 -->
+			<view class="floor">
+				<!-- 标题 -->
+				<view class="title">
+					<image src="/static/uploads/pic_floor01_title.png" />
+				</view>
+				<!-- 列表 -->
+				<view class="items">
+					<navigator url="">
+						<image src="/static/uploads/pic_floor01_1@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor01_2@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor01_3@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor01_4@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor01_5@2x.png" />
+					</navigator>
+				</view>
+			</view>
+			<!-- 户外运动 -->
+			<view class="floor">
+				<!-- 标题 -->
+				<view class="title">
+					<image src="/static/uploads/pic_floor02_title.png" />
+				</view>
+				<!-- 列表 -->
+				<view class="items">
+					<navigator url="">
+						<image src="/static/uploads/pic_floor02_1@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor02_2@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor02_3@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor02_4@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor02_5@2x.png" />
+					</navigator>
+				</view>
+			</view>
+			<!-- 箱包配饰 -->
+			<view class="floor">
+				<!-- 标题 -->
+				<view class="title">
+					<image src="/static/uploads/pic_floor03_title.png" />
+				</view>
+				<!-- 列表 -->
+				<view class="items">
+					<navigator url="">
+						<image src="/static/uploads/pic_floor03_1@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor03_2@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor03_3@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor03_4@2x.png" />
+					</navigator>
+					<navigator url="">
+						<image src="/static/uploads/pic_floor03_5@2x.png" />
+					</navigator>
+				</view>
+			</view>
+		<view>
 	</view>
 </template>
 
@@ -85,6 +164,42 @@
 		navigator {
 			width: 128rpx;
 			height: 140rpx;
+		}
+	}
+	.floor {
+		.title {
+			width: 750rpx;
+			height: 60rpx;
+			padding: 20rpx 0 0 10rpx;
+			background-color: #F4F4F4;
+		}
+		.items {
+			padding: 20rpx 16rpx;
+			// 解决塌陷问题
+			overflow: hidden;
+			navigator {
+				float: left;
+				padding-left: 10rpx;
+				&:first-child {
+					width: 232rpx;
+					height: 386rpx;
+					padding-left: 0;
+				}
+				&:nth-child(2),
+				&:nth-child(5){
+					width: 273rpx;
+					height: 188rpx;
+				}
+				&:nth-child(3),
+				&:nth-child(4){
+					width: 193rpx;
+					height: 188rpx;
+				}
+				// 五个图片中,倒数第一个和倒数第二个
+				&:nth-last-child(-n+2){
+					padding-top: 10rpx;
+				}
+			}
 		}
 	}
 </style>
