b77bb16f033262de46dfa1fe34431c2d6cdd5c42
小孩 <2232122044@qq.com>
2020-03-12 15:38:27 +0800

    商品详情页面-获取真实数据并填充

--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -1,19 +1,183 @@
 <template>
-  <div>商品详情页面+{{id}}</div>
+  <view class="wrapper">
+    <!-- 商品图片 -->
+    <swiper class="pics" indicator-dots indicator-color="rgba(255, 255, 255, 0.6)" indicator-active-color="#fff">
+      <swiper-item v-for="item in goods.pics" :key="item.goods_id">
+        <image :src="item.pics_big_url"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 基本信息 -->
+    <view class="meta">
+      <view class="price">￥{{goods.goods_price}}</view>
+      <view class="name">{{goods.goods_name}}</view>
+      <view class="shipment">快递:{{goods.goods_state===2?'免邮费':'不包邮'}} </view>
+      <text class="collect icon-star">收藏</text>
+    </view>
+    <!-- 商品详情 -->
+    <view class="detail">
+      <view :style='{fontSize: 0}' v-html="goods.goods_introduce"></view>
+      <!-- <rich-text :style='{fontSize: 0}' :nodes='goods.goods_introduce'></rich-text> -->
+    </view>
+    <!-- 操作 -->
+    <view class="action">
+      <button open-type="getPhoneNumber" class="icon-handset">联系客服</button>
+      <text class="cart icon-cart" @click="goCart">购物车</text>
+      <text class="add" @click='addCart'>加入购物车</text>
+      <text class="buy" @click="createOrder">立即购买</text>
+    </view>
+  </view>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      id: ""
+  export default {
+    data () {
+      return {
+        // 当前商品的id
+        id: '',
+        // 当前商品的所有信息
+        goods:null
+      }
+    },
+    onLoad(param){
+      this.id = param.id
+      this.loadData(param.id)
+    },
+    methods: {
+      // 获取商品的详细数据
+      async loadData(id){
+        const {message} = await this.$request({
+          path: 'goods/detail',
+          param:{
+            goods_id :id
+          }
+        })
+        this.goods = message
+      }
     }
-  },
-  onLoad(param) {
-    this.id = param.id;
   }
-};
 </script>
 
-<style lang="less" scoped>
+<style scoped lang="less">
+  .lazyimg {
+    background-color: pink;
+    p {
+      margin: 0;
+      padding: 0;
+    }
+    a {
+      margin: 0;
+      padding: 0;
+    }
+    image {
+      vertical-align: middle;
+    }
+  }
+  .wrapper {
+    margin-bottom: 100rpx;
+    background-color: #f4f4f4;
+  }
+  .pics {
+    height: 640rpx;
+  }
+  
+  .meta {
+    height: 250rpx;
+    line-height: 1;
+    padding: 30rpx 180rpx 30rpx 20rpx;
+    box-sizing: border-box;
+    background-color: #fff;
+    position: relative;
+    .price {
+      font-size: 36rpx;
+      color: #ea4451;
+      margin-bottom: 20rpx;
+    }
+    .name {
+      color: #333;
+      line-height: 1.4;
+      font-size: 33rpx;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      overflow: hidden;
+    }
+    .shipment {
+      font-size: 27rpx;
+      color: #999;
+      position: absolute;
+      bottom: 30rpx;
+    }
+    .collect {
+      width: 140rpx;
+      height: 88rpx;
+      text-align: center;
+      box-sizing: border-box;
+      border-left: 1rpx solid #ddd;
+      font-size: 24rpx;
+      color: #999;
+      position: absolute;
+      right: 10rpx;
+      top: 91rpx;
+    }
+    [class*="icon-"]::before {
+      display: block;
+      font-size: 45rpx;
+      margin-bottom: 10rpx;
+    }
+  }
+  .detail image {
+    width: 100%;
+    height: 480rpx;
+    margin-top: 20rpx;
+    background-color: blue;
+  }
+  .action {
+    width: 100%;
+    height: 98rpx;
+    background-color: #fff;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    align-items: center;
+    text {
+      display: block;
+    }
+    .add, .buy {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 210rpx;
+      text-align: center;
+      font-size: 27rpx;
+      color: #fff;
+    }
+    .add {
+      background-color: #f4b73f;
+    }
+    .buy {
+      background-color: #ea4451;
+    }
+    button {
+      padding: 0;
+      border-radius: 0;
+      background-color: #fff;
+      &::after {
+        border: none;
+      }
+    }
+    button, .cart {
+      flex: 1;
+      text-align: center;
+      color: #989898;
+      font-size: 24rpx;
+      box-sizing: border-box;
+    }
+    [class*="icon"]::before {
+      display: block;
+      font-size: 45rpx;
+      margin-bottom: 2rpx;
+    }
+  }
 </style>
