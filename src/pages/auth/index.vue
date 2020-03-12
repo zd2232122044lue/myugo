0ef91458e1c0fa13859571be1e95b85991dcd8f3
小孩 <2232122044@qq.com>
2020-03-12 21:59:33 +0800

    购物车页面-结算-获取微信的授权信息和code码

--- src/pages/auth/index.vue
+++ src/pages/auth/index.vue
@@ -1,9 +1,30 @@
 <template>
-  <div>授权登录</div>
+  <view>
+    <button type="primary" open-type="getUserInfo" @getUserInfo="getUserInfo">点击授权</button>
+  </view>
 </template>
 
 <script>
   export default {
+      methods:{
+          getUserInfo(e){
+              // 1.获取微信的授权信息
+              const {
+                encryptedData,
+                iv,
+                rawData,
+                signature 
+              } = e.detail
+              // 2.获取code
+              uni.login({
+                  success: (res) =>{
+                      const {code} = res
+                  }
+              })
+              // 3.调用后台接口获取token信息
+              // 4.获取token后,进行缓存,然后跳回到上一个页面
+          }
+      }
 }
 </script>
 
